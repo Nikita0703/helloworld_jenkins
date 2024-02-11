de215b5fd9b6f055639b3129281872f05d486309
Nikita0703 <nikita.noviczenko2005@gmail.com>
2024-02-11 22:01:40 +0300

    adding docker hub to jenkins

--- src/main/java/com/example/helloworld_jenkins/controller/MainController.java
+++ src/main/java/com/example/helloworld_jenkins/controller/MainController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1")
 public class MainController {
+
     @GetMapping
     public String hello(){
         return "hello,world";
     }
+
 }
